89aa482ec80c0ecab8d088bae55b634d5bbd28b5
kwinczewski <146000852+kwinczewski@users.noreply.github.com>
2024-02-15 20:14:00 +0000

    Update

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -25,7 +25,7 @@ export class TasksComponent implements OnInit {
   }
 
   toggleReminder(task: Task) {
-    task.reminder = !task.reminder
+    task.reminder = !task.reminder;
     this.taskService.updateTaskReminder(task).subscribe()
   }
 }
